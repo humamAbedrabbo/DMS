e42ea7eaa3866b40de27afa01f2dc3a1dc89ff8e
Humam <humam.abedrabbo@hotmail.com>
2019-12-17 19:54:07 +0200

    Add project files.

---
+++ DMS/Data/ApplicationDbContext.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace DMS.Data
+{
+    public class DmsDbContext : IdentityDbContext
+    {
+        public DmsDbContext(DbContextOptions<DmsDbContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            // Identity Configuration
+            builder.Entity<IdentityUser>().ToTable("Users");
+            builder.Entity<IdentityUserToken<string>>().ToTable("UserTokens");
+            builder.Entity<IdentityUserLogin<string>>().ToTable("UserLogins");
+            builder.Entity<IdentityRole>().ToTable("Roles");
+            builder.Entity<IdentityUserRole<string>>().ToTable("UserRoles");
+            builder.Entity<IdentityUserClaim<string>>().ToTable("UserClaims");
+            builder.Entity<IdentityRoleClaim<string>>().ToTable("RoleClaims");
+        }
+    }
+}

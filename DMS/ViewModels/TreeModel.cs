ae9af1548b533c39a7653307eca19882b5081368
Humam <humam.abedrabbo@hotmail.com>
2019-12-21 21:12:04 +0200

    Add Folder  Details Page

--- DMS/ViewModels/TreeModel.cs
+++ DMS/ViewModels/TreeModel.cs
@@ -16,6 +16,7 @@ namespace DAS.ViewModels
         public string Title { get; set; }
         public int? ParentId { get; set; }
         public TreeModel Parent { get; set; }
+        public string Url => Type == TreeNodeType.Folder ? $"folders/{Id}" : "#";
         public Dictionary<string, string> Meta { get; set; }
         public List<TreeModel> Childs { get; set; }
     }

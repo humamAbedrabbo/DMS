e7bc8b8aa762a7371083d683a60469d6cbcb9b5e
Humam <humam.abedrabbo@hotmail.com>
2019-12-21 05:42:24 +0200

    Add Repo Details Page

---
+++ DMS/ViewModels/MetaItemEditModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DAS.ViewModels
+{
+    public class MetaItemEditModel
+    {
+        public string Name { get; set; }
+        public string Value { get; set; }
+    }
+}

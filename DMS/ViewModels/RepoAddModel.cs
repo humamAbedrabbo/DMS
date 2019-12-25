4cc9b6ebddbc15ca1275ec53ef0e91fd830793fb
Humam <humam.abedrabbo@hotmail.com>
2019-12-25 03:36:35 +0200

    Add New Repository Validation

--- DMS/ViewModels/RepoAddModel.cs
+++ DMS/ViewModels/RepoAddModel.cs
@@ -1,5 +1,6 @@
 ﻿using DAS.Models;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,9 +8,11 @@ namespace DAS.ViewModels
 {
     public class RepoAddModel
     {
+        [Required]
         public string Name { get; set; }
         public string Title { get; set; }
         public string Description { get; set; }
+        [Required]
         public StorageType Storage { get; set; }
         public string Path { get; set; }
         public string UserName { get; set; }

784ecfa37d3589d536c70781199bbc83f3a1789b
Humam <humam.abedrabbo@hotmail.com>
2019-12-27 22:24:17 +0200

    Added dynamic meta fields and fix toast styles

--- DMS/Models/MetaField.cs
+++ DMS/Models/MetaField.cs
@@ -1,4 +1,6 @@
-﻿namespace DAS.Models
+﻿using System.Text;
+
+namespace DAS.Models
 {
     public class MetaField
     {
@@ -10,6 +12,65 @@
         public string DefaultValue { get; set; }
         public string Group => (Name ?? string.Empty).Split('.')[0];
         public string FieldTypeName => FieldType.ToString();
+        public string FieldFormat
+        {
+            get
+            {
+                if (FieldType == FieldType.Date)
+                    return "yyyy-MM-dd";
+                else
+                    return null;
+            }
+        }
+        public string FieldTag
+        {
+            get
+            {
+                switch(FieldType)
+                {
+                    case FieldType.List:
+                        return "select";
+                    case FieldType.LargeText:
+                        return "textarea";
+                    default:
+                        return "input";
+                }
+            }
+        }
+        public string FieldTagInputType
+        {
+            get
+            {
+                switch (FieldType)
+                {
+                    case FieldType.Number:
+                    case FieldType.Decimal:
+                        return "number";
+                    case FieldType.Date:
+                        return "date";
+                    default:
+                        return "text";
+                }
+            }
+        }
+        public string FieldTagInputSelectItems
+        {
+            get
+            {
+                if (FieldType == FieldType.List && !string.IsNullOrEmpty(ListSource))
+                {
+                    var sb = new StringBuilder();
+                    foreach (var item in ListItems)
+                    {
+                        sb.AppendLine($"<option value='{item}'>{item}</option>");
+                    }
+                    return sb.ToString();
+                }
+
+                return "";
+            }
+        }
+        public string[] ListItems => ListSource?.Split(',');
     }
 
     public enum FieldType
@@ -19,8 +80,6 @@
         Number,
         Decimal,
         Date,
-        DateTime,
-        Boolean,
         List
     }
 }

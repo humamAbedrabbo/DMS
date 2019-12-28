5a3942ffccabd7090983b0b8eaeefe7a16bea49e
Humam <humam.abedrabbo@hotmail.com>
2019-12-28 05:20:06 +0200

    Added File Size Text Utility

---
+++ DMS/Utils/FileSizeText.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DAS.Utils
+{
+    public static class FileSizeText
+    {
+        public static string ToString(long length)
+        {
+            string[] sizes = { "B", "KB", "MB", "GB", "TB" };
+            double len = length;
+            int order = 0;
+            while (len >= 1024 && order < sizes.Length - 1)
+            {
+                order++;
+                len = len / 1024;
+            }
+
+            // Adjust the format string to your preferences. For example "{0:0.#}{1}" would
+            // show a single decimal place, and no space.
+            return String.Format("{0:0.##} {1}", len, sizes[order]);
+        }
+    }
+}
